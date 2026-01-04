b8a01c0b1a1dd3e874b40ceabc9bcedb95e1ab5e
Alphazng <90766194+Alphazng@users.noreply.github.com>
2026-01-05 01:22:02 +0700

    sukses commit

---
+++ Page/__init__.py
@@ -0,0 +1,8 @@
+"""
+Page modules for QuakeAceh application
+
+This package contains the page modules:
+- beranda.py: Landing page with map
+- data.py: Data upload and validation
+- estimasi.py: PGA estimation with GMPE and ML
+"""
